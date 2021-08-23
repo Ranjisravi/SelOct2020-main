630ffd49285b1d07b880d13ee1e8b504a307ca8b
Ranjith Ravichandhran <82880737+Ranjisravi@users.noreply.github.com>
2021-08-23 10:44:53 +0530

    Add files via upload

---
+++ week1/day2/OfficeCar.java
@@ -0,0 +1,33 @@
+package week1.day2;
+
+import week1.day1.Car;
+import week3.day1.Vehicle;
+
+public class OfficeCar extends Vehicle {
+
+	public static void main(String[] args) {
+		
+		
+		  OfficeCar oc = new OfficeCar();
+		  
+		  oc.applyBrake();
+		 
+		
+		/*
+		 * Vehicle veh = new Vehicle();
+		 * 
+		 * veh.applyBrake();
+		 */
+		 
+		
+		/*
+		 * Car myCar = new Car();
+		 * 
+		 * myCar.driveCar(); myCar.applyBrake();
+		 * 
+		 * System.out.println(myCar.carPrice);
+		 */
+		
+	}
+
+}
